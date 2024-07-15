a69d302459750482a071d22d8202d38bb689bbaa
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-07-15 16:43:13 +0100

    Set up bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button className="btn-danger">Here</Button>
+    </div>
+  );
 };
 
 export default App;
